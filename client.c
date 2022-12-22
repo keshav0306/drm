8ea0a9dd5ec61fcfa6456c4c28bf0d88c12a860e
Keshav Gupta <102588113+keshav0306@users.noreply.github.com>
2022-12-23 00:16:44 +0530

    Added sparkle

--- client.c
+++ client.c
@@ -9,7 +9,7 @@
 
 #include "list.h"
 #include "client_include.h"
-
+#include "sparkle.h"
 
 int main(int argc, char ** argv){
 	int handle = connect_to_server("127.0.0.1");
@@ -18,14 +18,18 @@ int main(int argc, char ** argv){
 		exit(1);
 	}
 	map_window(window, handle);
+	struct context * context = new_context(window->height, window->width, window->addr);
+	memset(window->addr, 255, window->size);
 	while(1){
 		struct event * event = get_current_event(window, handle);
+		draw_point(context, 2, 2, 0xFF00FF00);
+		draw_line(context, 10, 20, 30, 30, 0xFFFF0000);
 		if(event){
 			if(event->key == 48){
 				unmap_window(window, handle);
 			}
 		}
-		memset(window->addr, 255, window->size);
+		
 		sleep(1);
 	}
 }
