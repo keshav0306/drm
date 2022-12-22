ccc5d2641a860f19191cb1e475d6aca8eb81b1c1
Keshav Gupta <102588113+keshav0306@users.noreply.github.com>
2022-12-23 01:10:51 +0530

    Update sparkle.c

--- sparkle.c
+++ sparkle.c
@@ -1,25 +1,44 @@
 #include "sparkle.h"
+#include <math.h>
 
 struct context * new_context(int height, int width, char * addr){
     struct context * context = (struct context *)malloc(sizeof(struct context));
     context->height = height;
-    context->height = width;
+    context->width = width;
     context->addr = addr;
     return context;
 }
 
-void draw_point(struct context * context, int x, int y, int colour){
+int draw_point(struct context * context, int x, int y, int colour){
     char * addr = context->addr;
     int height = context->height;
     int width = context->width;
     int * pixel = (int *) addr;
+    if(x < 0 || y < 0 || x > width || y > height){
+        return -1;
+    }
     *(pixel + y * width + x) = colour;
 }
 
-void draw_line(struct context * context, int x1, int y1, int x2, int y2, int colour){
-    int slope = (y2 - y1) / (x2 - x1);
+int draw_line(struct context * context, int x1, int y1, int x2, int y2, int colour){
+    float slope = (y2 - y1) / (float)(x2 - x1);
     for(int i=x1;i<=x2;i++){
-        int y = (i - x1) * slope + y1;
-        draw_point(context, i, y, colour);
+        float y = (i - x1) * slope + y1;
+        int gif = y;
+        int num = gif;
+        if(y - gif >= 0.5){
+            num++;
+        }
+        draw_point(context, i, num, colour);
+    }
+}
+
+int draw_circle(struct context * context, int x, int y, int radius, int colour){
+    for(int i = x - radius; i < x + radius; i++){
+        int base = sqrt((radius * radius) - (i - x) * (i - x));
+        int y1 = base + y1;
+        int y2 = (-1 * base) + y1;
+        draw_point(context, i, y1, colour);
+        draw_point(context, i, y2, colour);
     }
 }
