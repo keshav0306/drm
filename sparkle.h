93fc2c368fd7d7a72b1b9154c48df506fdc7e831
Keshav Gupta <102588113+keshav0306@users.noreply.github.com>
2022-12-23 01:11:07 +0530

    Update sparkle.h

--- sparkle.h
+++ sparkle.h
@@ -6,6 +6,8 @@ struct context{
 
 struct context * new_context(int height, int width, char * addr);
 
-void draw_point(struct context * context, int x, int y, int colour);
+int draw_point(struct context * context, int x, int y, int colour);
 
-void draw_line(struct context * context, int x1, int y1, int x2, int y2, int colour);
+int draw_line(struct context * context, int x1, int y1, int x2, int y2, int colour);
+
+int draw_circle(struct context * context, int x, int y, int radius);
