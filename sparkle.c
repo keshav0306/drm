32277e10aa69d0ebb08941926c34ce530139708d
Keshav Gupta <102588113+keshav0306@users.noreply.github.com>
2022-12-23 15:45:07 +0530

    Update sparkle.c

--- sparkle.c
+++ sparkle.c
@@ -1,5 +1,7 @@
 #include "sparkle.h"
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 struct context * new_context(int height, int width, char * addr){
     struct context * context = (struct context *)malloc(sizeof(struct context));
@@ -20,7 +22,36 @@ int draw_point(struct context * context, int x, int y, int colour){
     *(pixel + y * width + x) = colour;
 }
 
-int draw_line(struct context * context, int x1, int y1, int x2, int y2, int colour){
+int draw_line_mid_pt(struct context * context, int x1, int y1, int x2, int y2, int colour){
+    // if the slope is less than 1 and is positive then, we have to find for a current pixel
+    // as (x0, y0), what the next pixel will be, either (x0 + 1, y0) or (x0 + 1, y0 +1)
+    // the deciding parameter p(k+1) = p(k) + 2del(y) - 2del(x)(y(k+1) - y(k))
+    // p0 = 2del(y) - del(x)
+    int delx = x2 - x1;
+    int dely = y2 - y1;
+    int add1 = 2 * dely;
+    int add2 = 2 * dely - 2 * delx;
+    float slope = (y2 - y1) / (float)(x2 - x1);
+    int y = y1;
+    if (slope < 1){
+        draw_point(context, x1, y1, colour);
+        int p = 2 * dely - delx;
+        for(int i=x1 + 1;i<=x2;i++){
+            if(p<0){
+                p = p + add1;
+                draw_point(context, i, y, colour);
+            }
+            else{
+                p = p + add2;
+                draw_point(context, i, y++, colour);
+            }
+
+        }
+    }
+    
+}
+
+int draw_line_simple(struct context * context, int x1, int y1, int x2, int y2, int colour){
     float slope = (y2 - y1) / (float)(x2 - x1);
     for(int i=x1;i<=x2;i++){
         float y = (i - x1) * slope + y1;
@@ -33,11 +64,15 @@ int draw_line(struct context * context, int x1, int y1, int x2, int y2, int colo
     }
 }
 
+int draw_line_simple(struct context * context, int x1, int y1, int x2, int y2, int colour){
+    return draw_line_mid_pt(context, x1, y1, x2, y2 ,colour);
+}
+
 int draw_circle(struct context * context, int x, int y, int radius, int colour){
     for(int i = x - radius; i < x + radius; i++){
         int base = sqrt((radius * radius) - (i - x) * (i - x));
-        int y1 = base + y1;
-        int y2 = (-1 * base) + y1;
+        int y1 = base + y;
+        int y2 = (-1 * base) + y;
         draw_point(context, i, y1, colour);
         draw_point(context, i, y2, colour);
     }
