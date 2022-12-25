b10bfe30ae2b1830e25707e8968eb45e5e500ff6
Keshav Gupta <102588113+keshav0306@users.noreply.github.com>
2022-12-25 21:45:25 +0530

    added font

--- sparkle.h
+++ sparkle.h
@@ -11,3 +11,5 @@ int draw_point(struct context * context, int x, int y, int colour);
 int draw_line(struct context * context, int x1, int y1, int x2, int y2, int colour);
 
 int draw_circle(struct context * context, int x, int y, int radius, int colour);
+
+int draw_text(struct context * context, char * string, int x, int y, int colour);
